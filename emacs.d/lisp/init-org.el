1bc665c1980ee1d05875e16eeac68055b5683a92
latrokles <latrokles@gmail.com>
2020-05-11 22:26:11 -0700

    clean up org capture
    finally set up org-protocol to capture links from a bookmarklet in
    firefox.
    
    following:
    https://blog.aaronbieber.com/2016/11/24/org-capture-from-anywhere-on-your-mac.html
    
    1. https://github.com/aaronbieber/org-protocol-handler
    2. move Org Protocol Handler.app to /Applications
    3. created a bookmark in my bookmarks toolbar in firefox with the
    following url:
    `javascript:(function ()
    {window.location.href='org-protocol://capture://bp/'+encodeURIComponent(window.location.href)+'/'+encodeURIComponent(document.title)+'/'+encodeURIComponent(window.getSelection());
    })();`
    
    then just had to make some changes in my bookmark org-capture
    templates to get the right formatting. This one may still change a
    little bit.

--- emacs.d/lisp/init-org.el
+++ emacs.d/lisp/init-org.el
@@ -27,39 +27,37 @@
 
   ;; set up org capture
   (setq org-capture-templates
-	`(("p" "Capture personal items")
-	  ("pt" "Todo" entry
+	`(("t" "Capture Tasks")
+	  ("tp" "Todo (self)" entry
 	   (file+headline "org-self/todo.org" "Tasks")
 	   "* TODO %?\n %i\n %a")
-	  ("pn" "Notes" entry
+	  ("tw" "Todo (work)" entry
+	   (file+headline "org-work/todo.org" "Tasks")
+	   "* TODO %?\n %i\n %a")
+	  ("n" "Capture Notes")
+	  ("np" "Notes (self)" entry
 	   (file+datetree "org-self/notes.org")
 	   "* %?\nEntered on %U\n %i\n %a"
 	   :empty-lines 1)
-	  ("pb" "Bookmark" entry
-	   (file+datetree "org-self/bookmarks.org")
-	   ,(concat "* TODO %annotation\n"
-		    ":PROPERTIES:\n"
-		    ":CREATED: %u\n"
-		    ":END:\n\n"
-		    "%link\n\n"
-		    "%?")
-	   :empty-lines-after 1)
-	  ("w" "Capture work items")
-	  ("wt" "Todo" entry
-	   (file+headline "org-work/todo.org" "Tasks")
-	   "* TODO %?\n %i\n %a")
-	  ("wn" "Notes" entry
+	  ("nw" "Notes (work)" entry
 	   (file+datetree "org-work/notes.org")
 	   "* %?\nEntered on %U\n %i\n %a"
-	   :empty-lines-after 1)
-	  ("wb" "Bookmark" entry
-	   (file+datetree "org-work/bookmarks.org")
-	   ,(concat "* TODO %annotation\n"
+	   :empty-lines 1)
+	  ("b" "Capture Bookmarks")
+	  ("bp" "Bookmarks (self)" entry
+	   (file+datetree "org-self/bookmarks.org")
+	   ,(concat "* TODO %? %:description\n"
 		    ":PROPERTIES:\n"
 		    ":CREATED: %u\n"
-		    ":END:\n\n"
-		    "%link\n"
-		    "%?")
+		    ":END:\n")
+	   :empty-lines 1)
+	  ("bw" "Bookmarks (work)" entry
+	   (file+datetree "org-work/bookmarks.org")
+	   ,(concat "* TODO %? %:description\n"
+		    ":PROPERTIES:\n"
+		    ":CREATED: %u\n"
+		    ":END:\n"
+		    ":END:\n")
 	   :empty-lines 1))))
 
 (provide 'init-org)
