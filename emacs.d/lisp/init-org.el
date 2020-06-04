9e25613a0cbe74f879c751f07c05b38d20522613
latrokles <latrokles@gmail.com>
2020-06-03 21:50:11 -0700

    configure org-roam

--- emacs.d/lisp/init-org.el
+++ emacs.d/lisp/init-org.el
@@ -60,4 +60,28 @@
 		    ":END:\n")
 	   :empty-lines 1))))
 
+(use-package org-roam
+  :ensure t
+  :hook
+  (after-init . org-roam-mode)
+  :custom
+  (org-roam-directory "~/braindump")
+  :bind
+  (:map org-roam-mode-map
+	(("C-c n l" . org-roam)
+	 ("C-c n f" . org-roam-find-file)
+	 ("C-c n g" . org-roam-show-graph))
+	:map org-mode-map
+	(("C-c n i" . org-roam-insert))))
+
+(use-package deft
+  :ensure t
+  :after org
+  :bind ("C-c n d" . deft)
+  :custom
+  (deft-recursive t)
+  (deft-use-filter-string-for-filename t)
+  (deft-default-extension "org")
+  (deft-directory "~/braindump"))
+
 (provide 'init-org)
