19c6ff69fadb826b59e9f8db0bcf95e69f250fae
latrokles <latrokles@gmail.com>
2020-08-15 20:11:04 -0700

    switch some lisp things up
    1. use clozure cl as main cl implementation for now (may need some osx
    integration)
    2. add some leader key shortcuts for sly repl

--- emacs.d/lisp/init-cl.el
+++ emacs.d/lisp/init-cl.el
@@ -1,6 +1,22 @@
 ;;; init-cl.el
 
 ;;; Code:
+(defun latrokles/configure-lisp-leaders ()
+  "define leader keys for sly repl"
+  (evil-leader/set-key
+    "l" 'sly 				; start sly repl
+    "srl" 'sly-restart-inferior-lisp 	; restart sly
+    "sql" 'sly-quit-lisp 		; quit repl
+    "scr" 'sly-connect 			; connet to a running slynk session
+    "scd" 'sly-compile-defun 		; compile defun
+    "scf" 'sly-compile-file 		; compile file
+    "sed" 'sly-eval-defun 		; eval defun
+    "sel" 'sly-eval-last-expression 	; eval last sexpr
+    "suf" 'sly-undefine-function 	; undefine function
+    "swc" 'sly-who-calls 		; find who calls this function
+    "swu" 'sly-edit-uses 		; edit users of this symbol
+    ))
+
 (use-package lispy :ensure t)
 
 (use-package evil-lispy
@@ -14,9 +30,10 @@
 (use-package sly
   :ensure t
   :config
+  (latrokles/configure-lisp-leaders)
   (setq sly-lisp-implementations
-	`((sbcl ("/usr/local/bin/sbcl" "--noinform" "--no-linedit"))
-	  (ccl ("/usr/local/bin/ccl64")))))
+	`((ccl ("ccl64"))
+	  (sbcl ("sbcl" "--noinform" "--no-linedit")))))
 
 (use-package sly-quicklisp
   :ensure t
@@ -25,5 +42,6 @@
 (use-package sly-asdf
   :ensure t
   :after sly)
+
 
 (provide 'init-cl)
