babeb173dbd4c8cd4d64cd93a60ef8677152926f
latrokles <latrokles@gmail.com>
2023-06-14 15:01:56 -0700

    scorched earth approach to emacs config

--- emacs.d/lisp/init-unison.el
+++
@@ -1,8 +0,0 @@
-;;; init-unison.el
-(use-package unisonlang-mode
-  :ensure t
-  :config
-  (add-hook 'after-init-hook #'auto-revert-mode)
-  :mode (("\\.u\\'" . unisonlang-mode)))
-
-(provide 'init-unison)
