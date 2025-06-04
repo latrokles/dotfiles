5c76ebf9b97dd4906c1dc4810b4808fabb026a55
latrokles <latrokles@gmail.com>
2025-06-03 23:42:41 -0700

    some changes

--- config/phoenix.js
+++ config/phoenix.js
@@ -4,7 +4,7 @@ Phoenix.set({
   openAtLogin: true
 });
 
-// CONSTANTS 
+// CONSTANTS
 const HYPER = ['ctrl', 'alt', 'cmd'];
 const HYPER_SHIFT = ['ctrl', 'alt', 'cmd', 'shift'];
 const ALT_SHIFT = ['alt', 'shift'];
@@ -44,7 +44,43 @@ Window.prototype.toRight = function() {
   });
 };
 
+Window.prototype.firstQuadrant = function() {
+    this.setTopLeft({x: 0, y: 0});
+    this.setSize({
+	width: this.screen().frame().width / 2,
+	height: this.screen().frame().height / 2,
+    });
+};
+
+Window.prototype.secondQuadrant = function() {
+    this.setTopLeft({x: this.screen().frame().width / 2, y: 0});
+    this.setSize({
+	width: this.screen().frame().width / 2,
+	height: this.screen().frame().height / 2,
+    });
+};
+
+Window.prototype.thirdQuadrant = function() {
+    this.setTopLeft({x: 0, y: this.screen().frame().height / 2});
+    this.setSize({
+	width: this.screen().frame().width / 2,
+	height: this.screen().frame().height / 2,
+    });
+};
+
+Window.prototype.fourthQuadrant = function() {
+    this.setTopLeft({x: this.screen().frame().width / 2, y: this.screen().frame().height / 2});
+    this.setSize({
+	width: this.screen().frame().width / 2,
+	height: this.screen().frame().height / 2,
+    });
+};
+
 // POSITIONING
+Key.on('1', ALT_SHIFT, function() { Window.focused().firstQuadrant(); });
+Key.on('2', ALT_SHIFT, function() { Window.focused().secondQuadrant(); });
+Key.on('3', ALT_SHIFT, function() { Window.focused().thirdQuadrant(); });
+Key.on('4', ALT_SHIFT, function() { Window.focused().fourthQuadrant(); });
 Key.on('up', ALT_SHIFT, function() { Window.focused().toTop(); });
 Key.on('down', ALT_SHIFT, function() { Window.focused().toBottom(); });
 Key.on('left', ALT_SHIFT, function() { Window.focused().toLeft(); });
